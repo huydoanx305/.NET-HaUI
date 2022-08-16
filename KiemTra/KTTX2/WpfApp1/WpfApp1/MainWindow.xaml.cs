ce413ad17fde25ae97db5d674db9f4581cf84e88
Huy Doanx <huycntt03@gmail.com>
2022-08-16 07:04:20 +0700

    C# Haui

---
+++ KiemTra/KTTX2/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        static List<SinhVien> sinhViens = new List<SinhVien>();
+
+        private void Them_Click(object sender, RoutedEventArgs e)
+        {
+            String ma = masv.Text.ToString();
+            String name = hoten.Text.ToString();
+            String khoa = khoatt.Text.ToString();
+
+            if(ma == "" || name == "" || khoa == "")
+            {
+                MessageBox.Show("Mã, họ tên, khoa/trung tâm của SV không được rỗng!");
+                return;
+            }
+
+            foreach(SinhVien sv in sinhViens)
+            {
+                if(sv.maSV.Equals(ma))
+                {
+                    MessageBox.Show("Mã SV không được trùng nhau!");
+                    return;
+                }
+            }
+
+            int slxs;
+            if(!int.TryParse(solanxs.Text, out slxs) || int.Parse(solanxs.Text.ToString()) < 1)
+            {
+                MessageBox.Show("Số lần xuất sắc phải là kiểu số nguyên và > 0!");
+                return;
+            }
+
+            SinhVien sinhVien = new SinhVien();
+            sinhVien.maSV = ma;
+            sinhVien.hoTen = name;
+            sinhVien.soLanXS = slxs;
+            sinhVien.khoaTT = khoa;
+
+            if(slxs >= 5)
+            {
+                sinhVien.tienThuong = 500000;
+            } else if (slxs > 1 && slxs < 5)
+            {
+                sinhVien.tienThuong = 200000;
+            } else
+            {
+                sinhVien.tienThuong = 0;
+            }
+
+            sinhViens.Add(sinhVien);
+            listSV.Items.Add(sinhVien);
+        }
+
+        private void Tim_Click(object sender, RoutedEventArgs e)
+        {
+            listSV.Items.Clear();
+            foreach(SinhVien sv in sinhViens)
+            {
+                if(sv.khoaTT.Equals("1. Khoa Công nghệ thông tin"))
+                {
+                    listSV.Items.Add(sv);
+                }
+            }
+        }
+
+        private void listSV_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item = listSV.SelectedItem as SinhVien;
+            masv.Text = item.maSV;
+            hoten.Text = item.hoTen;
+            solanxs.Text = item.soLanXS.ToString();
+            khoatt.Text = item.khoaTT;
+        }
+    }
+}
---
+++ KiemTra/KTTX2/WpfApp1/WpfApp1/SinhVien.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp1
+{
+    class SinhVien
+    {
+        public String maSV { get; set; }
+        public String hoTen { get; set; }
+        public int soLanXS { get; set; }
+        public String khoaTT { get; set; }
+        public double tienThuong { get; set; }
+
+    }
+}
